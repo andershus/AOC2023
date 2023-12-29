5793e71a037e9231fb56c488713014f27ea70123
andershus <ahu084@gmail.com>
2023-12-29 16:51:03 +0100

    day22: init

---
+++ day22/src/main.rs
@@ -0,0 +1,34 @@
+use std::collections::HashMap;
+
+fn part1(input: &str) -> usize {
+    input.len()
+}
+
+fn part2(input: &str) -> usize {
+    input.len()
+}
+
+fn main() {
+    let binding =
+        std::fs::read_to_string("./input.txt").expect("Should have been able to read the file");
+    let contents = binding.trim_start_matches("\n").trim_end_matches("\n");
+    println!("result part1: {}", part1(&contents));
+    println!("result part2: {}", part2(&contents));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1("1,0,1~1,2,1
+0,0,2~2,0,2
+0,2,3~2,2,3
+0,0,4~0,2,4
+2,0,5~2,2,5
+0,1,6~2,1,6
+1,1,8~1,1,9"), 5);
+    }
+
+}
