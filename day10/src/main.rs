47c7c67145b5f84c1d55b06f6a3a907443ddde9e
andershus <ahu084@gmail.com>
2023-12-29 16:33:15 +0100

    day10: partial solve

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,21 +1,57 @@
-// use std::collections::BTreeMap;
+use std::collections::HashMap;
 
-fn part1(input: &str) -> u32 {
-    let chars: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
-    println!("{:?}", chars);
-    0
+fn part1(input: &str, starts: Vec<((usize, usize), (isize, isize), usize)>) -> usize {
+    let mut start_pos = (0, 0);
+    let grid: Vec<Vec<char>> = input
+        .lines()
+        .enumerate()
+        .map(|(i, line)| {
+            line.chars()
+                .enumerate()
+                .map(|(j, c)| {
+                    if c == 'S' {
+                        start_pos = (j, i);
+                    }
+                    c
+                })
+                .collect()
+        })
+        .collect();
+    dbg!(start_pos);
+    let mut seen = HashMap::new();
+    // should find these programatically
+    let mut queue = starts;
+    loop {
+        let (pos, dir, dist) = queue.remove(0);
+        seen.insert(pos, dist);
+        let next_dir = match grid[pos.1][pos.0] {
+            '|' | '-' => dir,
+            'L' | 'J' if dir.1 == 0 => (0, -1),
+            'L' | 'F' if dir.0 == 0 => (1, 0),
+            '7' | 'F' if dir.1 == 0 => (0, 1),
+            '7' | 'J' if dir.0 == 0 => (-1, 0),
+            _ => panic!("not implemented for {} at {:?}", grid[pos.1][pos.0], pos),
+        };
+        let next_pos = (pos.0.checked_add_signed(next_dir.0).unwrap(), pos.1.checked_add_signed(next_dir.1).unwrap());
+        // dbg!(&seen, &next);
+        if seen.contains_key(&next_pos) {
+            break;
+        }
+        queue.push((next_pos, next_dir, dist + 1));
+    }
+    *seen.iter().map(|(_,v)| v).max().unwrap()
+
 }
 
-fn part2(input: &str) -> i32 {
-    let mut result = 0;
-    result
+fn part2(input: &str) -> usize {
+    input.len()
 }
 
 fn main() {
     let binding =
         std::fs::read_to_string("./input.txt").expect("Should have been able to read the file");
     let contents = binding.trim_start_matches("\n").trim_end_matches("\n");
-    println!("result part1: {}", part1(&contents));
+    println!("result part1: {}", part1(&contents, vec![((90, 97), (0isize, -1isize), 1), ((90, 99), (0isize, 1isize), 1)]));
     println!("result part2: {}", part2(&contents));
 }
 
@@ -23,53 +59,53 @@ fn main() {
 mod tests {
     use super::*;
 
-    const TEST_INPUT: &str = ".....
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1(".....
 .S-7.
 .|.|.
 .L-J.
-.....";
+.....", vec![((1, 2), (0isize, 1isize), 1), ((2, 1), (1isize, 0isize), 1)]), 4);
+    }
 
-    const TEST_INPUT_2: &str = "-L|F7
+    #[test]
+    fn test_part12() {
+        assert_eq!(part1("-L|F7
 7S-7|
 L|7||
 -L-J|
-L|-JF";
+L|-JF", vec![((1, 2), (0isize, 1isize), 1), ((2, 1), (1isize, 0isize), 1)]), 4);
+    }
 
-    const TEST_INPUT3: &str = "..F7.
+    #[test]
+    fn test_part13() {
+        assert_eq!(part1("..F7.
 .FJ|.
 SJ.L7
 |F--J
-LJ...";
+LJ...", vec![((1, 2), (1isize, 0isize), 1), ((0, 3), (0isize, 1isize), 1)]), 8);
+    }
 
-    const TEST_INPUT_4: &str = "7-F7-
+    #[test]
+    fn test_part14() {
+        assert_eq!(part1("7-F7-
 .FJ|7
 SJLL7
 |F--J
-LJ.LJ";
+LJ.LJ", vec![((1, 2), (1isize, 0isize), 1), ((0, 3), (0isize, 1isize), 1)]), 8);
+    }
 
 
     #[test]
-    fn test_part1() {
-        assert_eq!(part1(&TEST_INPUT), 4);
+    fn test_part2() {
+        assert_eq!(part2("...........
+.S-------7.
+.|F-----7|.
+.||.....||.
+.||.....||.
+.|L-7.F-J|.
+.|..|.|..|.
+.L--J.L--J.
+..........."), 4);
     }
-
-    // #[test]
-    // fn test_part12() {
-    //     assert_eq!(part1(&TEST_INPUT), 4);
-    // }
-
-    // #[test]
-    // fn test_part13() {
-    //     assert_eq!(part1(&TEST_INPUT), 8);
-    // }
-
-    // #[test]
-    // fn test_part14() {
-    //     assert_eq!(part1(&TEST_INPUT), 8);
-    // }
-
-    // #[test]
-    // fn test_part2() {
-    //     assert_eq!(part2(TEST_INPUT), 2);
-    // }
 }
