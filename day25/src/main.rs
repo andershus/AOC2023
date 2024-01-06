9d4223b69c1ec22fe987ac3831b1ced9f5466c8b
andershus <ahu084@gmail.com>
2024-01-06 12:05:53 +0100

    day25: solve

--- day25/src/main.rs
+++ day25/src/main.rs
@@ -1,11 +1,37 @@
-// use std::collections::HashMap;
+use std::collections::HashMap;
+use std::iter::FromIterator;
+
+use rustworkx_core::connectivity::stoer_wagner_min_cut;
+use rustworkx_core::petgraph::graph::{NodeIndex, UnGraph};
+use rustworkx_core::Result;
 
 fn part1(input: &str) -> usize {
-    input.len()
-}
+    let mut graph: UnGraph<(), ()> = UnGraph::new_undirected();
+    let mut map: HashMap<&str, (NodeIndex, Vec<&str>)> =
+        HashMap::from_iter(input.lines().map(|line| {
+            let (vertex, edges) = line.split_once(": ").unwrap();
+            (
+                vertex,
+                (graph.add_node(()), edges.split_whitespace().collect()),
+            )
+        }));
+    for line in input.lines() {
+        for e in line.split_once(": ").unwrap().1.split_whitespace() {
+            if map.get(e).is_none() {
+                map.insert(e, (graph.add_node(()), Vec::new()));
+            }
+        }
+    }
+    let graph_len = map.len();
+    for (_, (v_i, edges)) in map.iter() {
+        for e in edges.iter() {
+            graph.update_edge(*v_i, map.get(e).unwrap().0, ());
+        }
+    }
+    let min_cut_res: Result<Option<(usize, Vec<_>)>> = stoer_wagner_min_cut(&graph, |_| Ok(1));
 
-fn part2(input: &str) -> usize {
-    input.len()
+    let partition = min_cut_res.unwrap().unwrap().1;
+    partition.len() * (graph_len - partition.len())
 }
 
 fn main() {
@@ -13,7 +39,6 @@ fn main() {
         std::fs::read_to_string("./input.txt").expect("Should have been able to read the file");
     let contents = binding.trim_start_matches("\n").trim_end_matches("\n");
     println!("result part1: {}", part1(&contents));
-    println!("result part2: {}", part2(&contents));
 }
 
 #[cfg(test)]
@@ -22,7 +47,9 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        assert_eq!(part1("jqt: rhn xhk nvd
+        assert_eq!(
+            part1(
+                "jqt: rhn xhk nvd
 rsh: frs pzl lsr
 xhk: hfx
 cmg: qnr nvd lhk bvb
@@ -34,7 +61,9 @@ ntq: jqt hfx bvb xhk
 nvd: lhk
 lsr: lhk
 rzs: qnr cmg lsr rsh
-frs: qnr lhk lsr"), 54);
+frs: qnr lhk lsr"
+            ),
+            54
+        );
     }
-
 }
